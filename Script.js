26d9d7c525b88703f3860018601d33999fbf8626
Javier Matias Insaurralde <javiermatias115@gmail.com>
2024-05-09 21:54:30 -0300

    se añade js ,  cambio de maquetacion y rutas a

---
+++ Script.js
@@ -0,0 +1,10 @@
+// Despliega el menu de usuario
+    function desplegarMenu() {
+        const menuUsuario = document.getElementById("menu-usuario");
+        menuUsuario.style.display = (menuUsuario.style.display === "block") ? "none" : "block";
+    }
+//Oculta el menu cunado deja de hacer hover sobre el
+    function ocultarMenu() {
+        const menuUsuario = document.getElementById("menu-usuario");
+        menuUsuario.style.display = "none";
+    }
