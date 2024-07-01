9eab7c32ec2d57f8af54c7dabe7ac29c5ff65c03
Javier Matias Insaurralde <javiermatias115@gmail.com>
2024-06-30 21:18:17 -0300

    actualizo fetch

--- static/scripts/ObtenerLibros.js
+++ static/scripts/ObtenerLibros.js
@@ -1,34 +1,7 @@
-// URL base da API
-const baseURL = 'https://back-end-trabajo-en-grupo-codo-a-codo.onrender.com/book/';
-
-function obtenerLibros() {
-    return fetch(baseURL)
-
-        .then(response => response.json())
-        .catch(error => {
-            console.error('Error al ingresar al endpoint', error);
-        });
-
-   
-} 
-
-obtenerLibros()
-.then(data=>{console.log( data +"log mati" );})
-.catch(   error => console.log(error)
-);
-
-
-
-
-fetch(baseURL , {
-
-    method: 'GET',
-    mode: 'cors', // asegúrate de que esté en 'cors' y no en 'no-cors'
-    headers: {
-      'Content-Type': 'application/json'
-    }
-
-  })
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.error('Error:', error));
+fetch("https://back-end-trabajo-en-grupo-codo-a-codo.onrender.com/book")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    }).catch((err) => {
+        console.log(err);
+    })
